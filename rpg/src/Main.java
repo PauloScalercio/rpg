29131de4db76cf172f9388c6724fb2f6512d5236
Paulo Scalercio <paulo.scalercio@uniriotec.br>
2018-07-17 19:59:07 -0300

    Main.java
    Commit Main

---
+++ rpg/src/Main.java
@@ -0,0 +1,9 @@
+import GalaxyFighter.GalaxyFighter;
+import engine.window.Game;
+
+public class Main {
+    public static void main(String[] args) {
+        Game g = new GalaxyFighter();
+        g.open();
+    }
+}
