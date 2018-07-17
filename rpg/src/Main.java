a66acb3963154e1323025606245aa0fd89eb8059
Jefferson Elbert <jefferson.simoes@uniriotec.br>
2018-07-17 20:01:39 -0300

    comentário (galaxy fighter)

--- rpg/src/Main.java
+++ rpg/src/Main.java
@@ -3,6 +3,7 @@ import engine.window.Game;
 
 public class Main {
     public static void main(String[] args) {
+        // test: galaxy fighter
         Game g = new GalaxyFighter();
         g.open();
     }
